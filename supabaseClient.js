cd731f34f5a185ce65efda6e7053bfecda50791f
juanchoparra-spec <juanchoparra@gmail.com>
2026-06-13 23:41:48 -0400

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://xhmcroyrjkgbcpwahyia.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_5TAyOpJwK4K7gkB4nfhPzQ_TtaHXBw-'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
